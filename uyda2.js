d830e151ce9e4e61fccb740fa77ddd143297a0f3
Joardan000 <Joardan000@gmail.com>
2025-06-09 15:05:39 +0500

    uyda2

---
+++ uyda2.js
@@ -0,0 +1,54 @@
+// function Talaba(ism,sinfi,yoshi){
+//     this.ism=ism;
+//     this.sinfi=sinfi;
+//     this.yoshi=yoshi;
+// }
+// let malumot = new Talaba("Javohir", 10, 17)
+// console.log(malumot)
+
+// function turtburchak (tomon1){
+//     this.tomoni = tomon1
+// }
+// let tomonlar = new turtburchak(4)
+// turtburchak.prototype.peremetr= function(){
+//     return this.tomoni * 4
+// }
+// turtburchak.prototype.yuzi= function(){
+//     return this.tomoni * this.tomoni
+// }
+// console.log(`P: ` + tomonlar.peremetr())
+// console.log(`S: ` + tomonlar.yuzi())
+
+// Talaba.prototype.getInfo = function (){
+//     console.log("Salom mening ismim " + this.ism + " sinfim " + this.sinfi + " yoshim " + this.yoshi)
+// }
+// let talaba1 = new Talaba("Javohir",10,17)
+// talaba1.getInfo()
+
+// let sonlar = [1, 3, 7, 8, 10];
+// Array.prototype.oxirgi = function () {
+//     return this[this.length - 1];
+// }
+// console.log(sonlar.oxorgi())
+
+// let a = prompt('Ism kiriting!')
+// function boshi (){
+//     return a[0].toUpperCase()
+// }
+// console.log(boshi())
+
+// let a = prompt("Enter your age!")*1
+// function foydalanuvchi (a){
+//     this.age = a;
+// }
+// let user = new foydalanuvchi(a)
+// foydalanuvchi.prototype.isAdult = function(){
+//     if (this.age >= 18){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+// console.log(user.isAdult())
+
+
